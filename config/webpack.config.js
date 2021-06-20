6cb4538412384620819438b951b14b245f21c4b1
Edin93 <il_matador@hotmail.fr>
2021-06-20 22:44:09 +0100

    [config] webpack for simple react app to test the whole configuration

---
+++ config/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    hot: true,
+    port: 5000,
+    contentBase: path.resolve(__dirname, 'src'),
+  },
+  plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './dist/index.html',
+      title: 'Digital Factory - React Test',
+    }),
+	],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>HELLO WORLD</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
